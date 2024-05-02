5f899054e212937207ec9e23d3e6330f51e61f0d
faridkia <farid.1382.k@gmail.com>
2024-05-02 13:41:43 +0330

    RigisterView

--- accounts/forms.py
+++ accounts/forms.py
@@ -15,6 +15,21 @@ class LoginForm(forms.Form):
 
         return username
 
+
+class SignUpForm(forms.Form):
+    username = forms.CharField(max_length=30)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+
+        if len(username) < 4:
+            raise forms.ValidationError('طول نام کاربری بالای ۴ کاراکتر باشد')
+        elif len(username) > 30:
+            raise forms.ValidationError('طول نام کاربری کمتر از ۳۰ کاراکتر باشد')
+
+        return username
+
     def clean_password(self):
         password = self.cleaned_data.get('password')
         try:
